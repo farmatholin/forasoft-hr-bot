b75746bc7cde5b116f9f6ed94bc909fbbe3985a0
Vladislav <vladislav@fora-soft.com>
2016-10-01 15:55:15 +0300

    update

--- bot.py
+++ bot.py
@@ -4,12 +4,25 @@ import config
 hr_bot = telebot.TeleBot(config.BOT_TOKEN)
 
 
-# Handle '/start' and '/help'
-@hr_bot.message_handler(commands=['help', 'start'])
+# Handle '/start'
+@hr_bot.message_handler(commands=['start'])
 def send_welcome(message):
-    hr_bot.reply_to(message,
-                    ("Hi there, I am EchoBot.\n"
-                     "I am here to echo your kind words back to you."))
+    response_text = """
+    Привет, компания fora soft приветствует тебя.\n
+    Как мы могли уже заметить, у тебя накопилось много вопросов, не стесняйся, спрашивай.\n
+    with <3 Fora soft
+    """
+    hr_bot.reply_to(message, response_text)
+
+
+# Handle '/help'
+@hr_bot.message_handler(commands=['help'])
+def send_welcome(message):
+    response_text = """
+    /mama mama
+    /papa
+    """
+    hr_bot.reply_to(message, response_text)
 
 
 # Handle all other messages
