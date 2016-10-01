168fc74a10d180740d743780660bf53f4696c5d9
Vladislav <vladislav@fora-soft.com>
2016-10-01 13:44:01 +0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import flask
 import telebot
 import logging
-from bot import hr_bot
 import config
 import time
+
+bot = telebot.TeleBot(config.BOT_TOKEN)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -21,7 +22,7 @@ def webhook():
     if flask.request.headers.get('content-type') == 'application/json':
         json_string = flask.request.get_data().encode('utf-8')
         update = telebot.types.Update.de_json(json_string)
-        hr_bot.process_new_messages([update.message])
+        bot.process_new_messages([update.message])
         return ''
     else:
         flask.abort(403)
@@ -31,17 +32,24 @@ def getApp():
     return app
 
 
-# hr_bot.polling(none_stop=False, interval=0, timeout=20)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
-# Remove webhook, it fails sometimes the set if there is a previous webhook
 
-if __name__ == '__main__':
-    hr_bot.remove_webhook()
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    # hr_bot.polling(none_stop=False, interval=0, timeout=20)
 
-    # Set webhook
-    hr_bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-                       certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
-    app.run(host=config.WEBHOOK_LISTEN,
-            port=config.WEBHOOK_PORT,
-            ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
-            debug=True)
+    # Remove webhook, it fails sometimes the set if there is a previous webhook
+
+
+bot.remove_webhook()
+
+bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
+                certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
+app.run(host=config.WEBHOOK_LISTEN,
+        port=config.WEBHOOK_PORT,
+        ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
+        debug=True)
--- bot.py
+++ bot.py
@@ -4,12 +4,4 @@ import config
 hr_bot = telebot.TeleBot(config.BOT_TOKEN)
 
 
-@hr_bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    hr_bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@hr_bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    hr_bot.reply_to(message, message.text)
 
