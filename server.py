317ee5ab91c426158351c2e19df7dfac77bdb531
Vladislav <vladislav@fora-soft.com>
2016-10-01 14:29:01 +0300

    update

--- server.py
+++ server.py
@@ -2,24 +2,30 @@ import flask
 import telebot
 import logging
 import config
-import cherrypy
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
-# WebhookServer, process webhook calls
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-           'content-type' in cherrypy.request.headers and \
-           cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
+app = flask.Flask(__name__)
+
+## Empty webserver index, return nothing, just http 200
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+# Process webhook calls
+@app.route(config.WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().encode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_messages([update.message])
+        return ''
+    else:
+        flask.abort(403)
 
 
 # Handle '/start' and '/help'
@@ -43,13 +49,8 @@ bot.remove_webhook()
 bot.set_webhook(url=config.WEBHOOK_URL_BASE+config.WEBHOOK_URL_PATH,
                 certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
 
-# Start cherrypy server
-cherrypy.config.update({
-    'server.socket_host': config.WEBHOOK_LISTEN,
-    'server.socket_port': config.WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': config.WEBHOOK_SSL_PRIV
-})
-
-cherrypy.quickstart(WebhookServer(), config.WEBHOOK_URL_PATH, {'/': {}})
+# Start flask server
+app.run(host=config.WEBHOOK_LISTEN,
+        port=config.WEBHOOK_PORT,
+        ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
+        debug=True)
