014881423512d906e53f5e39ac3b67a61e7d830c
Vladislav <vladislav@fora-soft.com>
2016-10-01 14:19:46 +0300

    update

--- server.py
+++ server.py
@@ -2,55 +2,54 @@ import flask
 import telebot
 import logging
 import config
-import time
+import cherrypy
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-app = flask.Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
-
-
-@app.route(config.WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().encode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_messages([update.message])
-        return ''
-    else:
-        flask.abort(403)
+# WebhookServer, process webhook calls
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+           'content-type' in cherrypy.request.headers and \
+           cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
 
 
-def getApp():
-    return app
-
-
-@bot.message_handler(commands=['start', 'help'])
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
     bot.reply_to(message, message.text)
 
-    # hr_bot.polling(none_stop=False, interval=0, timeout=20)
 
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
 
-    bot.remove_webhook()
+# Set webhook
+bot.set_webhook(url=config.WEBHOOK_URL_BASE+config.WEBHOOK_URL_PATH,
+                certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
 
-    bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-                    certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host': config.WEBHOOK_LISTEN,
+    'server.socket_port': config.WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': config.WEBHOOK_SSL_PRIV
+})
 
-
-app.run(host=config.WEBHOOK_LISTEN,
-        port=config.WEBHOOK_PORT,
-        ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
-        debug=True)
+cherrypy.quickstart(WebhookServer(), config.WEBHOOK_URL_PATH, {'/': {}})
